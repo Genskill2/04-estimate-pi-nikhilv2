93fc42124411c09d8bc3243574162ed6841d786b
Nikhil V Gopal <nikhilvenu2000@gmail.com>
2021-05-30 16:44:19 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,22 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc_pi(int n) 
+{float c=0.0;
+float ans; 
+
+for(int i=1;i<=n;i++) 
+{ 
+float x= frandom(); 
+float y= frandom(); 
+printf("%f %f\n ",x,y);
+if(sqrt(x*x+y*y)<1) 
+c++; 
+} 
+
+ans = 4*c/n;
+return ans; 
+}
 
 float frandom() {
   long int q = random();
